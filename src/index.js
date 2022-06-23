141e357e77d87c3ccac4e4c8aabc45dba13c1a05
Nastya Filippova <nastya.filippova87@gmail.com>
2022-06-23 23:00:26 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    
+    n = (Math.abs(n)).toString();
+    let reverseNumber ='';
+    for (let i = n.length - 1; i >= 0; i--) {
+      reverseNumber = reverseNumber + n[i]; 
+    }
+    return reverseNumber;
 }
